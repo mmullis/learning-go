77eb7f81564f8e13c3a6eefe3d28892b8efa5805
Michael Mullis <mmullis@yp.com>
2017-07-20 18:13:35 +0000

    add events_processor; add unbuffered channel example

--- event-streams/events_processor.go
+++ event-streams/events_processor.go
@@ -7,20 +7,30 @@ import (
 	"log"
 )
 
+type Event struct {
+	id int64
+}
+
+/* Goal is to push events onto event channel for processing
+*/
 func main() {
 	asIsLog := log.New(os.Stderr, "", 0)
 	detailedLog := log.New(os.Stderr, "", log.Ldate|log.Ltime|log.LUTC|log.Lmicroseconds|log.Llongfile)
 
 	bufferedCh := make(chan int, 3)
 	unBufferedCh := make(chan int)
+	eventCh := make(chan Event, 20)
 
 	go func() {
 		for i := 0; i < 100; i++ {
 			bufferedCh <- i
 			unBufferedCh <- i*1000
+			event := Event{id: int64(i*1000000)}
+			eventCh <- event
 		}
 		close(bufferedCh)
 		close(unBufferedCh)
+		close(eventCh)
 	}()
 
 	var wg sync.WaitGroup
@@ -40,6 +50,15 @@ func main() {
                         fmt.Printf("unbufferedCh: %d\n", v)
                 }
         }()
+
+        wg.Add(1)
+        go func() {
+                defer wg.Done()
+                for v := range eventCh {
+                        fmt.Printf("eventCh: %V\n", v)
+                }
+        }()
+
 
 	wg.Wait()
 
