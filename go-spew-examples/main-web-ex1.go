9fb1d5dccbc283ea8d72acddfec20de30d8d96fa
Michael Mullis <mmullis@yp.com>
2017-11-02 12:49:53 +0000

    add go-spew examples

---
+++ go-spew-examples/main-web-ex1.go
@@ -0,0 +1,24 @@
+// go get -u github.com/davecgh/go-spew/spew
+
+package main
+
+import (
+    "fmt"
+    "html"
+    "net/http"
+
+    "github.com/davecgh/go-spew/spew"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html")
+    fmt.Fprintf(w, "Hi there, %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "<!--\n" + html.EscapeString(spew.Sdump(w)) + "\n-->")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
+
+
